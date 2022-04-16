f9bdc0e2faab235455952a0e2f9575b35b24231f
Benjamin Fazli <72983346+bfzli@users.noreply.github.com>
2022-04-16 18:21:38 +0200

    Delete cvitae directory

--- cvitae/src/layouts/AuthLayout.jsx
+++
@@ -1,5 +0,0 @@
-import { container } from '../constants/styles/modules/auth/AuthLayout.module.scss';
-
-export default function AuthLayout({ children }) {
-  return <main className={container}>{children}</main>;
-}
