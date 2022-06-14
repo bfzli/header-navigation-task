3f55c62921fe2970dbc05c570e9cfacb443a34e4
Benjamin Fazli <bfzli@hotmail.com>
2022-06-14 15:25:37 +0200

    bfzli:add | adding some pages

--- cvitae/src/Router.jsx
+++
@@ -1,5 +0,0 @@
-import Auth from "./pages/Auth";
-
-export default function Router() {
-  return <Auth />;
-}
--- cvitae/src/views/auth/AuthView.jsx
+++
@@ -1,5 +0,0 @@
-import styles from '../../constants/styles/modules/auth/AuthView.module.scss';
-
-export default function AuthView() {
-  return <section className={styles.container}>Authview</section>;
-}
